ddc5a9f02643a5c6b9d2a1495b93040d793329a3
Minsu <86006389+minsu20@users.noreply.github.com>
2022-06-30 03:02:02 +0900

    [LeetCode-876] Middle of the Linked List

---
+++ 1주차/[LeetCode - 876] Middle of the Linked List/[LeetCode - 876] 김민수.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        list_length=0
+        temp=head
+        while temp is not None:
+            temp=temp.next
+            list_length+=1
+        
+        for i in range(list_length/2):
+            head=head.next
+    
+        
+        return head
