037f818e3d5211b33bc3fdb0e8f6b81b60c3ea6e
Minsu <86006389+minsu20@users.noreply.github.com>
2022-06-30 03:04:47 +0900

    [LeetCode - 1337] The K Weakest Rows in a Matrix

--- 1주차/[LeetCode - 1337] The K Weakest Rows in a Matrix/[LeetCode - 1337] 김민수.py
+++ 1주차/[LeetCode - 1337] The K Weakest Rows in a Matrix/[LeetCode - 1337] 김민수.py
@@ -5,18 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        dict={} # 1 개수 담는 dict key는 row 번호, value는 1 개수
+        dict={} 
+        
         for i in range(len(mat)):
-            num=0 #num는 1개수
+            num=0 
             for j in range(len(mat[i])):
                 if mat[i][j]==1:
                     num+=1
             dict[i]=num
         
-        #dictonary value로 sorted 정렬
         sorted_tuple=sorted(dict.items(), key=lambda x:x[1])
-
-        #k까지 첫 번째 원소만 리스트로 반환
+        
         result_list=[]
         for i in range(0,k):
             result_list.append(sorted_tuple[i][0])
